534e99972abdc825c47cc6672c6498d4791e4976
Lua <130156447+Mr-LuaM@users.noreply.github.com>
2023-09-24 22:26:14 +0800

    V3
    FINAL

--- app/Views/main.php
+++ app/Views/main.php
@@ -11,50 +11,86 @@
 
 
 
-  <!-- In your view where the search form is displayed -->
-  <form action="/search" method="get">
-    <input type="search" name="search" placeholder="Search song">
-    <input type="hidden" name="context" value="<?= $context ?>">
+  <div class="container">
+    <a href="/main" class="btn btn-success float-end ">
+      <i class="fas fa-home"></i> Home
+    </a>
+    <br>
+    <!-- Search Form -->
+    <form class="mt-4 mb-3" action="/search" method="get">
+      <div class="input-group">
+        <input type="search" name="search" class="form-control" placeholder="Search song">
+        <input type="hidden" name="context" value="<?= $context ?>">
+        <?php if ($context === 'playlist'): ?>
+          <input type="hidden" id="playlistIDInput" name="playlistID" value="">
+        <?php endif ?>
+        <div class="input-group-append">
+          <button type="submit" class="btn btn-success">Search</button> <!-- Applied a green color -->
+        </div>
+      </div>
+    </form>
 
-    <?php if ($context === 'playlist'): ?>
-      <!-- Include the playlistID parameter when in a playlist context -->
-      <input type="hidden" name="playlistID" value="<?= $playlist['playlist_id'] ?>">
-    <?php endif ?>
+    <!-- Music Player Header -->
+    <h1 class="mt-4">Music Player</h1>
 
-    <button type="submit" class="btn btn-primary">Search</button>
-  </form>
+    <!-- Action Buttons -->
+    <div class="mb-3">
+      <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">My
+        Playlist</button> <!-- Applied a green color -->
+      <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#manageSongsModal">Manage
+        Songs</button> <!-- Applied a green color -->
+    </div>
 
-  <h1>Music Player</h1>
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    My Playlist
-  </button>
+    <!-- Audio Player -->
+    <div class="audio-controls mb-3">
+      <audio id="audio" controls autoplay></audio>
+    </div>
 
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#manageSongsModal">
-    Manage Songs
-  </button>
+    <!-- Music Playlist -->
+    <ul class="list-group" id="playlist">
+      <?php foreach ($music as $musicItem): ?>
+        <li class="list-group-item d-flex justify-content-between align-items-center"
+          data-src="<?= base_url('/uploads/songs/' . $musicItem['file_path']) ?>">
+          <?= $musicItem['title'] ?>
+          <?php if ($context === 'playlist'): ?>
+            <div class="btn-group">
+              <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal"
+                onclick="setMusicID('<?= $musicItem['music_id'] ?>')">
+                <i class="fas fa-plus"></i>+
+              </button>
+              <a href="<?= site_url('/removeFromPlaylist/' . $musicItem['id']) ?>" class="btn btn-danger btn-sm">
+                <i class="fas fa-minus"></i>-
+              </a>
+            </div>
+          <?php else: ?>
+            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal"
+              onclick="setMusicID('<?= $musicItem['music_id'] ?>')">
+              <i class="fas fa-plus"></i>+
+            </button>
+          <?php endif ?>
+        </li>
+      <?php endforeach ?>
+    </ul>
 
-  <audio id="audio" controls autoplay></audio>
-  <ul id="playlist">
-    <?php foreach ($music as $musicItem): ?>
-      <li data-src="<?= base_url('/uploads/songs/' . $musicItem['file_path']) ?>">
-        <?= $musicItem['title'] ?>
-      </li>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal"
-        onclick="setMusicID('<?= $musicItem['music_id'] ?>')">
-        <i class="fas fa-plus"></i>+
-      </button>
-    <?php endforeach ?>
-  </ul>
 
 
 
+  </div>
 
 
+  <script>
+    document.addEventListener('DOMContentLoaded', function () {
+      // Get the playlist ID from the URL
+      const urlParams = new URLSearchParams(window.location.search);
+      const playlistID = urlParams.get('playlistID');
 
-
-
-
-
+      // Set the value of the hidden input field
+      const playlistIDInput = document.getElementById('playlistIDInput');
+      if (playlistID) {
+        playlistIDInput.value = playlistID;
+      }
+    });
+  </script>
 
   <script>
     $(document).ready(function () {
@@ -126,6 +162,7 @@
       document.getElementById('musicID').value = musicID;
     }
   </script>
+
 </body>
 
 </html>
